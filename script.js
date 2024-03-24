94926325b974cd6fdeb2a3f703b57557e784cbd8
Zoe <159435044+Mortiton@users.noreply.github.com>
2024-03-24 22:20:33 +0000

    Add clear function to clear button
    Clears the textcontent of the calcDisplay element

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const numBtns = document.querySelectorAll()
+//DOM DECLARATIONS
+const calDisplay = document.querySelector("#calc-display")
+const clearBtn = document.querySelector("#clear")
+
+//FUNCTIONS
+
+//Set the display to zero
+function clearDisplay() {
+    calDisplay.textContent=0;
+}
+
+//EVENT LISTENERS
+clearBtn.addEventListener("click", clearDisplay);
