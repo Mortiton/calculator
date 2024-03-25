f9ea08d69617ae63315c2b5d2a3008a012dd9097
Zoe <159435044+Mortiton@users.noreply.github.com>
2024-03-25 01:53:44 +0000

    Add click event to number buttons
    Added a click event to the number buttons. They display the button value
    to the calculator display when pressed.

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 //DOM DECLARATIONS
-const calDisplay = document.querySelector("#calc-display")
-const clearBtn = document.querySelector("#clear")
-
+const calDisplay = document.querySelector("#calc-display");
+const clearBtn = document.querySelector("#clear");
+const numBtns = document.querySelectorAll(".num");
+let noInputs = true;
 //FUNCTIONS
 
 //Set the display to zero
 function clearDisplay() {
-    calDisplay.textContent=0;
+  calDisplay.textContent = 0;
+  noInputs = true;
 }
 
+//number event listner
+function numPressed(event) {
+  if (noInputs) {
+    calDisplay.textContent= "";
+    noInputs = false;
+  }
+  calDisplay.textContent += event.target.value;
+}
 //EVENT LISTENERS
 clearBtn.addEventListener("click", clearDisplay);
+
+numBtns.forEach((btn) => {
+  btn.addEventListener("click", numPressed);
+});
