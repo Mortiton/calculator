f1963003a6f7aa18dd9172ff1ffc4163fd0021ad
Zoe <159435044+Mortiton@users.noreply.github.com>
2024-03-25 02:25:51 +0000

    Add click event for operators

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const calDisplay = document.querySelector("#calc-display");
 const clearBtn = document.querySelector("#clear");
 const numBtns = document.querySelectorAll(".num");
 const opBtns = document.querySelectorAll(".op")
+const equalBtn = document.querySelector("#equal")
 let noInputs = true;
-let firstNumber = null;
-let secondNumber = null;
+let inputNumbers = []
 //FUNCTIONS
 
 //Set the display to zero
 function clearDisplay() {
   calDisplay.textContent = 0;
   noInputs = true;
+  //clear any inputted numbers
+  inputNumbers = [];
 }
 
 //number event listner
@@ -23,10 +25,22 @@ function numPressed(event) {
   calDisplay.textContent += Number(event.target.value);
 }
 
+//Saves the content of the calculator display to an array and then clears the display content
+function opClicked(){
+    let saveContent = new Promise((resolve, reject)=> {
+            inputNumbers.push(calDisplay.textContent);
+            resolve();
+    })
+    saveContent.then(()=> {
+        calDisplay.textContent ='';
+    })
+}
+
 //calculation function
 //needs two numbers for an input and an operator. 
 //Get the numbers from the calDisplay
 function calculate(operator) {
+    let total = null;
 }
 //EVENT LISTENERS
 clearBtn.addEventListener("click", clearDisplay);
@@ -34,3 +48,7 @@ clearBtn.addEventListener("click", clearDisplay);
 numBtns.forEach((btn) => {
   btn.addEventListener("click", numPressed);
 });
+
+opBtns.forEach(op => {
+    op.addEventListener("click", opClicked);
+})
