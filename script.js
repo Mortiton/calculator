4986f608c7f23ccaafdbacde892e98566cb34fb4
Zoe <159435044+Mortiton@users.noreply.github.com>
2024-03-25 03:23:29 +0000

    Add the calculate function

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 const calDisplay = document.querySelector("#calc-display");
 const clearBtn = document.querySelector("#clear");
 const numBtns = document.querySelectorAll(".num");
-const opBtns = document.querySelectorAll(".op")
-const equalBtn = document.querySelector("#equal")
+const opBtns = document.querySelectorAll(".op");
+const equalBtn = document.querySelector("#equal");
 let noInputs = true;
-let inputNumbers = []
+let inputNumbers = [];
+let inputOp = [];
 //FUNCTIONS
 
 //Set the display to zero
@@ -14,34 +15,69 @@ function clearDisplay() {
   noInputs = true;
   //clear any inputted numbers
   inputNumbers = [];
+  inputOp = [];
 }
 
-//number event listner
 function numPressed(event) {
   if (noInputs) {
-    calDisplay.textContent= "";
+    calDisplay.textContent = "";
     noInputs = false;
   }
   calDisplay.textContent += Number(event.target.value);
 }
 
-//Saves the content of the calculator display to an array and then clears the display content
-function opClicked(){
-    let saveContent = new Promise((resolve, reject)=> {
-            inputNumbers.push(calDisplay.textContent);
-            resolve();
-    })
-    saveContent.then(()=> {
-        calDisplay.textContent ='';
-    })
+function opClicked(event) {
+  inputOp.push(event.target.value);
+  calDisplay.textContent = "";
 }
 
-//calculation function
-//needs two numbers for an input and an operator. 
-//Get the numbers from the calDisplay
-function calculate(operator) {
-    let total = null;
+function opClicked(event) {
+  let saveContent = new Promise((resolve, reject) => {
+    inputNumbers.push(Number(calDisplay.textContent));
+    inputOp.push(event.target.value);
+    resolve();
+  });
+  saveContent.then(() => {
+    calDisplay.textContent = "";
+  });
 }
+
+//Iterates through the numbers array and applys the operator from the inputOps array
+function calculate(numbers, operators) {
+  let total = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    switch (operators[i - 1]) {
+      case "+":
+        total += numbers[i];
+        break;
+      case "-":
+        total -= numbers[i];
+        break;
+      case "*":
+        total *= numbers[i];
+        break;
+      case "/":
+        total /= numbers[i];
+        break;
+      default:
+        throw new Error(`Invalid operator: ${operators[i - 1]}`);
+    }
+  }
+
+  return total;
+}
+
+function equalClicked() {
+  //push last number on the display into the Numbers Input Array
+  inputNumbers.push(Number(calDisplay.textContent));
+  let result = calculate(inputNumbers, inputOp);
+  calDisplay.textContent = result;
+  //clear arrays
+  inputNumbers = [];
+  inputOp = [];
+}
+
 //EVENT LISTENERS
 clearBtn.addEventListener("click", clearDisplay);
 
@@ -49,6 +85,8 @@ numBtns.forEach((btn) => {
   btn.addEventListener("click", numPressed);
 });
 
-opBtns.forEach(op => {
-    op.addEventListener("click", opClicked);
-})
+opBtns.forEach((op) => {
+  op.addEventListener("click", opClicked);
+});
+
+equalBtn.addEventListener("click", equalClicked);
